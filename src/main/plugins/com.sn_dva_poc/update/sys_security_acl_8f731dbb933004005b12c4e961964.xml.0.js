815d67ab542e1f36293054deb367a4c32f102340
emelnik2 <emelnik@ametec.com.mx>
2025-11-07 09:43:08 -0600

    Delete src/main directory

--- src/main/plugins/com.sn_dva_poc/update/sys_security_acl_8f731dbb933004005b12c4e961964.xml.0.js
+++
@@ -1,7 +0,0 @@
-answer = false;
-
-if (gs.isLoggedIn())
-	answer = true;
-
-if (pm.isRegistered("com.glide.explicit_roles") && gs.getProperty("glide.security.use_explicit_roles", "false") == "true")
-	answer = true;
